0940844ed2f37c3e4c178144d1e32695377c11c3
Gabriel.Madichie <amadichie@yahoo.com>
2022-04-30 08:03:56 +0100

    Initialized the Application - Set server port to 8888 - ResolutionInitializer modified - Made the Application extends WebSecurityConfigurerAdapter - Created User and UserAuthority entities - Created a UserRepository interface which extends CrudRepository - Created a UserRepositoryUserDetailsService which implements UserDetailsService

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
@@ -1,4 +1,68 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+
+
+@Entity(name="authorities")
 public class UserAuthority {
+
+    @Id
+    UUID id;
+
+    @Column
+    String authority;
+
+    // ... getters and setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    // --------------------------------------
+
+    @JoinColumn(name="username", referencedColumnName="username")
+    @ManyToOne
+    User user;
+
+    // ...
+    /* Add two constructors, first the default one that JPA expects,
+    and then a convenience one that takes a user and an authority:
+     */
+
+    UserAuthority() {}
+
+    public UserAuthority(User user, String authority) {
+        this.id = UUID.randomUUID();
+        this.user = user;
+        this.authority = authority;
+    }
+
+//    JPA expects you to manage the bi-directional relationship between User and UserAuthority, so, lastly, add a Set<UserAuthority> field as well as a grantAuthority method to User,
+
+
+
 }
--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -1,4 +1,60 @@
 package io.jzheaux.springsecurity.resolutions;
 
-public class UserRepositoryUserDetailsService {
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
+
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+//    @Override
+//    public UserDetails loadUserByUsername(String username) {
+//        throw new UsernameNotFoundException("no user");
+//    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return this.users.findByUsername(username)
+                .map(BridgedUser::new)
+                .orElseThrow(() -> new UsernameNotFoundException("invalid user"));
+    }
+
+    private final UserRepository users;
+
+    public UserRepositoryUserDetailsService(UserRepository users) {
+        this.users = users;
+    }
+
+//    @Override
+//    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+//        return null;
+//    }
+
+    private static class BridgeUser extends User implements UserDetails {
+        public BridgeUser(User user) {
+            super(user);
+        }
+
+        public List<GrantedAuthority> getAuthorities() {
+            return this.userAuthorities.stream()
+                    .map(UserAuthority::getAuthority)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }
+
+        public boolean isAccountNonExpired() {
+            return this.enabled;
+        }
+
+        public boolean isAccountNonLocked() {
+            return this.enabled;
+        }
+
+        public boolean isCredentialsNonExpired() {
+            return this.enabled;
+        }
+    }
+
 }
